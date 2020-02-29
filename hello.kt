09fe914e53e11afaa3263043aecdc8c9d2ad978d
aresyuan11 <61619834+aresyuan11@users.noreply.github.com>
2020-02-29 19:49:13 +0800

    创建hello.kt
    第一次使用github创建文件

---
+++ hello.kt
@@ -0,0 +1,19 @@
+var _Hello = "Hello,world";
+
+const val userName="Ares";
+
+fun main(args:Array<String>) {
+    println("Hello,Kotlin");
+
+//    var student = Student("Lisa",15);
+//    println(student.toString());
+
+
+    _Hello = "Jack,rose";
+
+    var score : Float = 96.5f;
+    var y = 20
+    y = 100
+
+    val sex="man";
+}
